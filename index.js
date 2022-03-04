01b13d8aa50aa66a19065b3eb40e599988d86528
Thisseas X <thisseasx@yahoo.com>
2022-03-05 01:18:26 +0200

    Add comments

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
+/**
+ * Splits an array of numbers to multiple partitions of given size.
+ *
+ * @param {number[]} arr List of numbers
+ * @param {number} partitionSize Size of each partition
+ * @returns {number} Array of partitions
+ */
 const partition = (arr, partitionSize) => {
   const result = [];
-  let temp = [];
+  let buffer = [];
 
-  arr.forEach((i) => {
-    temp.push(i);
+  arr.forEach((number) => {
+    buffer.push(number);
 
-    if (i % partitionSize === 0) {
-      result.push(temp);
-      temp = [];
+    if (number % partitionSize === 0) {
+      result.push(buffer);
+      buffer = [];
     }
   });
 
-  if (temp.length > 0) {
-    result.push(temp);
+  if (buffer.length > 0) {
+    result.push(buffer);
   }
 
   return result;
 };
 
+// Long array of numbers to operate on
 const numbers = Array(32000000)
   .fill()
   .map((_, i) => i + 1);
 
+// The above array split into 8 equal-size partitions, one for each thread
 const splitNumbers = partition(numbers, numbers.length / 8);
 
+// Start performance timer
 console.time('time');
 
+// Make a new worker for each partition and have it calculate its sum
 const promises = splitNumbers.map(
   (nums) =>
     new Promise((resolve) => {
@@ -40,9 +51,11 @@ const promises = splitNumbers.map(
 );
 
 Promise.all(promises).then((results) => {
+  // Combine the sums of all workers
   const sum = results.reduce((a, b) => a + b);
 
   document.getElementById('data').textContent = sum;
 
+  // End performance timer
   console.timeEnd('time');
 });
